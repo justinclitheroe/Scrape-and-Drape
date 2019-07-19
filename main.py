18893ca50e6b86f0b5d29d190f1735b9ed88fdd7
justin clitheroe <justinclitheroe@gmail.com>
2019-07-19 16:05:15 -0400

    day 3 finished, I can't believe that someone wrote html this bad

--- main.py
+++ main.py
@@ -3,29 +3,42 @@ from utils import *
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
-# s3 Views
-#   -CB-GET
-#   -CB-SAVE
-#   -CB-DELETE
-#   -ERRORS
+"""
+This is just where things start, kinda ugly right now but I don't really see the point in cleaning it
+Everyone that looks into this will likely want to do different things with it so this is essentially
+a scratch work zone
 
-start = '2019-7-1'
+TODO make Documentation for useful filter names per app and how to use these base parts
+"""
+
+start = '2019-4-14'
 end = '2019-7-14'
 
-s3_frame = load_or_generate_frame("s3", "LOAD")
-s3_frame = final_touches(s3_frame,
-                         view_cols="CB",
-                         remove_cols="Error",
-                         start_date=start,
-                         end_date=end)
-s3_frame.plot()
+# s3_frame = load_or_generate_frame("s3", "LOAD")
+# s3_frame = final_touches(s3_frame,
+#                          view_cols="CB",
+#                          remove_cols="Error",
+#                          start_date=start,
+#                          end_date=end,
+#                          trim_outliers=True)
+# s3_frame.plot()
+#
+# lasso_frame = load_or_generate_frame("lasso", "LOAD")
+# lasso_frame = final_touches(lasso_frame,
+#                             remove_cols="Vulnerabilities_a",
+#                             view_cols="Login",
+#                             start_date=start,
+#                             end_date=end,
+#                             trim_outliers=True)
+# lasso_frame.plot()
 
-lasso_frame = load_or_generate_frame("lasso", "LOAD")
-lasso_frame = final_touches(lasso_frame,
-                            remove_cols="Vulnerabilities_a",
-                            view_cols="Login",
+mchec_frame = load_or_generate_frame("mchec", "GENERATE")
+mchec_frame = final_touches(mchec_frame,
+                            remove_cols="",
+                            view_cols="PlatformType",
                             start_date=start,
-                            end_date=end)
-lasso_frame.plot()
+                            end_date=end,
+                            trim_outliers=True)
+mchec_frame.plot()
 
 plt.show()
--- parsers.py
+++ parsers.py
@@ -1,6 +1,19 @@
+"""
+The parsers responsible for getting data from the html
+
+Frustratingly similar for what they do, lots of them are likely one offs because
+the reports don't follow any kind of uniform layout
+
+typically the parsers are fairly simple
+    Get rows
+    populate data in dict through those rows
+The data just happens to manifest itself in different ways
+"""
 
 
-# Parses the most general kind of table
+# Parses the most general kind of table, format: text/numeric data
+# It also can do multiple similarly titled ones in a row,
+# though that was only useful in lasso so far
 def parse_basic_tables(body, d, table_name, table_range=None, alt_title=""):
     if table_range is not None:
         for i in range(table_range[0], table_range[1]):
@@ -20,19 +33,53 @@ def parse_basic_tables(body, d, table_name, table_range=None, alt_title=""):
     return d
 
 
+
+
 # This parser works on a table with format text/numeric/numeric data
-def parse_mchec_req_res_table(body, d, table_name="reqRespTable", alt_title=""):
-    title = alt_title
+def parse_mchec_req_res_table(body, d, title, table_name):
     rows = body.findall('.//*[@id="{}"]/tbody/tr'.format(table_name))
     for j in rows:
-        d["{}_{}_{}".format(title, j[0].text_content(), j[1].text_content())] = int(j[2].text_content())
+        d["{}_REQ_{}".format(title, j[0].text_content())] = int(j[1].text_content())
+        d["{}_RES(ms)_{}".format(title, j[0].text_content())] = float(j[2].text_content())
+    return d
+
+
+def parse_mchec_component_table(body, d, title):
+    """
+    Okay here we go this parser is bonkers enough to get its own block documentation
+
+    because of what was likely an imperceptible error when writing on the log generation side of things,
+    after about 3 hours of reading tool documentation, I've realized that the problem is that there aren't any
+    </tr> closing tags, The solution would to lookahead, but the way that the library works is that it actually
+    ends the list at the closing tag, so they're pretty much unreachable
+
+    the likely solution is to write something to clean the files, which I was already probably gonna do to cut the
+    file sizes down
+    """
+    rows = body.findall('.//table[2]/tbody/tr')
+    component_name = ""
+    for j in rows:
+        index = 0
+        more = True
+        if j[0].text_content():
+            component_name = j[0].text_content()
+            print("new component detected changing to", component_name)
+        while more:
+            try:
+                print(j[(index * 4) + 0].text_content(), j[(index * 4) + 1].text_content(),
+                      j[(index * 4) + 2].text_content(), j[(index * 4) + 3].text_content())
+                index += 1
+            except:
+                more = False
+
+            # d["{}_{}{}REQ".format(title, j[0].text_content(), j[1].text_content())] = int(j[2].text_content())
+            # d["{}_{}{}REQ".format(title, j[0].text_content(), j[1].text_content())] = float(j[3].text_content())
     return d
 
 
 # This parser works on a table with format text/text/numeric data
-def parse_three_col_table(body, d, table_name, alt_title=""):
-    title = alt_title
+def parse_three_col_table(body, d, title, table_name):
     rows = body.findall('.//*[@id="{}"]/tbody/tr'.format(table_name))
     for j in rows:
-        d["{}_{}_{}".format(title, j[0].text_content(), j[1].text_content())] = int(j[2].text_content())
+        d["{}_{}".format(title, j[0].text_content())] = int(j[2].text_content())
     return d
